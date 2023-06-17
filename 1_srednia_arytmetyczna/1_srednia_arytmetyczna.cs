b22fcd599b788e3fad2212d7241ea2c9ff2ec2c3
Barlomiej100704 <129675337+Barlomiej100704@users.noreply.github.com>
2023-06-17 02:35:30 +0200

    1_srednia_arytmetyczna
    dodanie 1 zadania

---
+++ 1_srednia_arytmetyczna/1_srednia_arytmetyczna.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+public class MyProgram
+{
+	public static void Main(string[] args)
+	{
+		double a, b, c;
+        Console.WriteLine("Podaj a");
+        a = inputValue();
+        Console.WriteLine("Podaj b");
+        b = inputValue();
+        Console.WriteLine("Podaj c");
+        c = inputValue();
+		double sredniaArytmetyczna;
+		sredniaArytmetyczna = (a + b + c) / 3;
+		Console.WriteLine("Średnia arytmetyczna to " + sredniaArytmetyczna);
+	}
+
+	// .NET can only read single characters or entire lines from the
+	// console. The following function safely reads a double value.
+	private static double inputValue()
+	{
+		double result;
+		while (!double.TryParse(Console.ReadLine(), out result)) ;
+		return result;
+	}
+}
